c6c31269785a4eb2a651e68863bf32829a9e0130
Muhammad Bilal <bilal.se74@gmail.com>
2021-07-28 12:50:40 +0500

    added player motion with motion script

---
+++ FPS Multiplayer Game 2/Assets/Scripts/Motion.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.bilal.fps
+{
+    public class Motion : MonoBehaviour
+    {
+        //player movement speed
+        public float playerSpeed;
+        //player rigid body
+        private Rigidbody rig;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //disable main camera
+            Camera.main.enabled = false;
+
+            rig = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            float t_hmove = Input.GetAxis("Horizontal");
+            float t_vmove = Input.GetAxis("Vertical");
+
+            Vector3 t_direction = new Vector3(t_hmove, 0, t_vmove);
+            t_direction.Normalize();
+
+            rig.velocity = transform.TransformDirection(t_direction) * playerSpeed * Time.deltaTime;
+        }
+    }
+}
